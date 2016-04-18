1f499f25b718cf0a662f345260d37b6172d6328c
Sword Elucidator <nagisa940216@gmail.com>
2016-04-18 01:14:48 -0700

    start the game project

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+    public MovieTexture movTexture;
+
+    //whether movie is being drawn
+    public bool isDrawMov = true;
+
+    public bool isShowMessage = false;
+
+    public TweenScale logoTweenScale;
+
+	// Use this for initialization
+	void Start () {
+        movTexture.loop = false;
+        movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isDrawMov)
+        {
+            if (Input.GetMouseButtonDown(0) && isShowMessage == false)
+            {
+                isShowMessage = true;
+            }else if (Input.GetMouseButtonDown(0) && isShowMessage == true)
+            {
+                StopMov();
+            }
+        }
+       if (isDrawMov != movTexture.isPlaying)
+        {
+            StopMov();
+        }
+	}
+
+    void OnGUI()
+    {
+        if (isDrawMov) { 
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movTexture);
+            if (isShowMessage)
+            {
+                GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 40), "再次点击屏幕退出动画的播放");
+            }
+        }
+    }
+
+    private void StopMov()
+    {
+        movTexture.Stop();
+        isDrawMov = false;
+        //show main scene
+        logoTweenScale.PlayForward();
+    }
+}
